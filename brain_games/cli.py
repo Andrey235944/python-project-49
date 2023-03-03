16c4ffb281276193fbfb9c83af2bba1dc5dde9f4
Andrey Ivlev <ivlev-pm@mail.ru>
2023-03-03 11:07:43 +0300

    версия 2.2 brain even

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,6 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print('Hello, ' + name)
+    return name
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,37 @@
-import prompt
-import random
+#!/usr/bin/env python3
 
-print('Welcome to the Brain Games!')
-name = prompt.string('May I have your name? ')
-print('Hello, ' + name + '!')
+from brain_games import cli
+import random
+import prompt
+
+name = cli.welcome_user()
+
 print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
-i = 1
-b = 1
-y = 'yes'
-n = 'no'
-while i <= 4:
-    a = random.randint(1, 200)
-    print('Question: ' + str(a))
-    answer = prompt.string('Your answer: ')
-    i += i
-    if a % 2 == 0 and answer == y or a % 2 != 0 and answer == n:
-        print('Correct!')
-        b += 1
-        if b == 4:
-            print('Congratulations,' + name + '!')
-    if a % 2 == 0 and answer == n:
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-        print("Let's try again," + name + '!')
-        break
-    if a % 2 != 0 and answer != n:
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
-        print("Let's try again," + name + '!')
-        break
-    if a % 2 == 0 and answer != n:
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
-        print("Let's try again," + name + '!')
-        break
+def main():
+    i = 1
+    b = 1
+    y = 'yes'
+    n = 'no'
+    while i <= 4:
+        a = random.randint(1, 200)
+        print('Question: ' + str(a))
+        answer = prompt.string('Your answer: ')
+        i += i
+        if a % 2 == 0 and answer == y or a % 2 != 0 and answer == n:
+            print('Correct!')
+            b += 1
+            if b == 4:
+                print('Congratulations,' + name + '!')
+        if a % 2 == 0 and answer != y:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{y}'.")
+            print("Let's try again," + name + '!')
+            break
+        if a % 2 != 0 and answer != n:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{n}'.")
+            print("Let's try again," + name + '!')
+            break
+
+if __name__ == "__main__":
+    main()
